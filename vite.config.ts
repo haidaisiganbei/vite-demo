a7216cd97e81bd4eec19dc1c44b15fa48d7a7c9b
lin yijie <yijie_lin@fzzixun.com>
2022-05-06 10:45:17 +0800

    基础配置:使用unplugin-vue-components插件，自动引入以声明的vue组件

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()]
+    vue(),
+    // 生成compoennts.d.ts组件定义文件
+    // https://github.com/antfu/unplugin-vue-components
+    Components({})
+  ]
 })
