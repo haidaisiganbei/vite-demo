3cb646307e44e344b4abca609679ac2249f886a9
lin yijie <yijie_lin@fzzixun.com>
2022-05-06 11:22:39 +0800

    基础配置:使用unplugin-auto-import引入Vue,VueRouter,VueUse相关API

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,18 @@ export default defineConfig({
     vue(),
     // 生成compoennts.d.ts组件定义文件
     // https://github.com/antfu/unplugin-vue-components
-    Components({})
+    Components({}),
+    // 全局引入Vue相关API
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({ 
+      imports:[
+        // https://v3.cn.vuejs.org/api/
+        'vue',
+        // https://router.vuejs.org/zh/api/index.html
+        'vue-router',
+        // https://vueuse.org/functions.html
+        {'@vueuse/core':["useTitle","useStorage"]}
+      ]
+    }),
   ]
 })
